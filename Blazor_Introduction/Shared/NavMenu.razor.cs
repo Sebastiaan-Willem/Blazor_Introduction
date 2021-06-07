50df428caa9f957fb821fbf99bbd04673d342ee0
Seba Kesteloot <seba.kesteloot@gmail.com>
2021-06-07 10:16:32 +0200

    Add project files.

---
+++ Blazor_Introduction/Shared/NavMenu.razor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor_Introduction.Shared
+{
+    public partial class NavMenu
+    {
+        //code in partial class instead of the razor file "NavMenu.razor" to separate C# code from markup 
+
+        private bool collapseNavMenu = true;
+
+        private string NavMenuCssClass => collapseNavMenu ? "collapse" : null;
+
+        private void ToggleNavMenu()
+        {
+            collapseNavMenu = !collapseNavMenu;
+        }
+    }
+}
