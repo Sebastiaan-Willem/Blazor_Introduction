5daac999c1a884950a350cc2c2bc24c12f1b7bc7
Seba Kesteloot <seba.kesteloot@gmail.com>
2021-06-07 14:19:17 +0200

    Added css

---
+++ Blazor_Introduction/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor_Introduction.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public double Rating { get; set; }
+        public int YearOfRelease { get; set; }
+    }
+}
