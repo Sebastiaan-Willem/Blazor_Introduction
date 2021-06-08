7e782f4ccdfab23262d9a2b78e46de98e7162f9a
Seba Kesteloot <seba.kesteloot@gmail.com>
2021-06-08 11:29:04 +0200

    Add Movie werkt

--- Blazor_Introduction/Models/Movie.cs
+++ Blazor_Introduction/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,22 @@ namespace Blazor_Introduction.Models
     public class Movie
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Gelieve een titel in te voeren")]
         public string Title { get; set; }
-        public string Genre { get; set; }
+        
+        public int GenreId { get; set; }
+        public Genre Genre { get; set; }
+
+        [Required(ErrorMessage = "Gelieve een beschrijving in te voeren")]
+        public string Description { get; set; }
+
+       
+        public bool IsInTheaters { get; set; }
+
+        [Range(0,10, ErrorMessage = "Score tussen de 0 en 10")]
         public double Rating { get; set; }
-        public int YearOfRelease { get; set; }
+               
+        public DateTime ReleaseDate { get; set; }
     }
 }
--- Blazor_Introduction/Services/MovieService.cs
+++ Blazor_Introduction/Services/MovieService.cs
@@ -8,6 +8,13 @@ namespace Blazor_Introduction.Services
 {
     public class MovieService : IMovieService
     {
+        private IGenreService _genreService;
+        private List<Genre> genres;
+        public MovieService(IGenreService service)
+        {
+            _genreService = service;
+            genres = _genreService.GetGenres();
+        }
         //OnInitialized() -> similar to OnAwake function in unity
         //seed list with dummy data
         public async Task<List<Movie>> GetMoviesAsync()
@@ -17,9 +24,9 @@ namespace Blazor_Introduction.Services
 
             return new List<Movie>
             {
-                new Movie{Title = "Titanic", Genre="Drama", Rating=8.4, YearOfRelease=1999},
-                new Movie{Title = "Joker", Genre="Drama", Rating=8.1, YearOfRelease=2019},
-                new Movie { Title = "Ghost Ship", Genre = "Horror", Rating = 5.7, YearOfRelease = 2004 },
+                new Movie{Title = "Titanic", IsInTheaters = true, GenreId= 0 ,Genre= genres[0] , Rating=8.4, ReleaseDate = new DateTime(1999,01,01) },
+                new Movie{Title = "Joker", IsInTheaters = true, Genre= genres[4] , Rating=8.1, ReleaseDate = new DateTime(2019,01,01)},
+                new Movie { Title = "Ghost Ship", IsInTheaters = true, Genre= genres[3] , Rating = 5.7, ReleaseDate = new DateTime(2004,01,01) },
             };
         }
 
